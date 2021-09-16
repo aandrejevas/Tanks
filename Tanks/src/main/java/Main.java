a6dffa7dc0bf1255ba70460b19e54b8a95e8ebee
AAndrejevas <aandrejevas5@gmail.com>
2021-09-16 11:40:24 +0300

    correct networking

--- Tanks/src/main/java/Main.java
+++ Tanks/src/main/java/Main.java
@@ -1,5 +1,5 @@
 
-import java.util.Arrays;
+import java.nio.ByteBuffer;
 import processing.core.PApplet;
 import processing.net.Client;
 
@@ -7,12 +7,11 @@ import processing.net.Client;
 public class Main extends PApplet {
 
 	public static final int W = 600, H = 500;
+	public static final ByteBuffer buffer = ByteBuffer.allocate(16);
 
-	public Client c;
-	public String input;
-	public int data[];
+	public static Client c;
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		PApplet.main(Main.class, args);
 	}
 
@@ -24,26 +23,18 @@ public class Main extends PApplet {
 	@Override
 	public void setup() {
 		c = new Client(this, "127.0.0.1", 12345);
+		stroke(0);
 	}
 
 	@Override
 	public void draw() {
 		if (mousePressed == true) {
-			// Draw our line
-			stroke(255);
-			line(pmouseX, pmouseY, mouseX, mouseY);
-			// Send mouse coords to other person
-			c.write(pmouseX + " " + pmouseY + " " + mouseX + " " + mouseY + "\n");
+			c.write(buffer.putInt(0, pmouseX).putInt(4, pmouseY).putInt(8, mouseX).putInt(12, mouseY).array());
 		}
 
-		// Receive data from server
-		while (c.available() > 0) {
-			input = c.readString();
-			input = input.substring(0, input.indexOf("\n"));  // Only up to the newline
-			data = Arrays.stream(split(input, ' ')).mapToInt(a -> Integer.parseInt(a)).toArray(); // Split values into an array
-			// Draw line using received coords
-			stroke(0);
-			line(data[0], data[1], data[2], data[3]);
+		while (c.available() != 0) {
+			c.readBytes(buffer.array());
+			line(buffer.getInt(0), buffer.getInt(4), buffer.getInt(8), buffer.getInt(12));
 		}
 	}
 }
--- TanksServer/src/main/java/Main.java
+++ TanksServer/src/main/java/Main.java
@@ -1,5 +1,4 @@
 
-import java.util.Arrays;
 import processing.core.PApplet;
 import processing.net.Client;
 import processing.net.Server;
@@ -7,20 +6,17 @@ import processing.net.Server;
 // Server
 public class Main extends PApplet {
 
-	public static final int W = 600, H = 500;
+	public static final byte buffer[] = new byte[16];
 
-	public Server s;
-	public Client c;
-	public String input;
-	public int data[];
+	public static Server s;
+	public static Client c;
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		PApplet.main(Main.class, args);
 	}
 
 	@Override
 	public void settings() {
-		size(W, H);
 	}
 
 	@Override
@@ -30,24 +26,9 @@ public class Main extends PApplet {
 
 	@Override
 	public void draw() {
-		if (mousePressed == true) {
-			// Draw our line
-			stroke(255);
-			line(pmouseX, pmouseY, mouseX, mouseY);
-			// Send mouse coords to other person
-			s.write(pmouseX + " " + pmouseY + " " + mouseX + " " + mouseY + "\n");
-		}
-
-		// Receive data from client
-		c = s.available();
-		if (c != null) {
-			input = c.readString();
-			println(input);
-			input = input.substring(0, input.indexOf("\n"));  // Only up to the newline
-			data = Arrays.stream(split(input, ' ')).mapToInt(a -> Integer.parseInt(a)).toArray(); // Split values into an array
-			// Draw line using received coords
-			stroke(0);
-			line(data[0], data[1], data[2], data[3]);
+		while ((c = s.available()) != null) {
+			c.readBytes(buffer);
+			s.write(buffer);
 		}
 	}
 }
