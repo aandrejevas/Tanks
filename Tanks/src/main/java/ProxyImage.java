f56d8e6835e964ae04fb4922dfb68d26185a3696
AAndrejevas <aandrejevas5@gmail.com>
2021-11-22 21:34:33 +0200

    proxy done

---
+++ Tanks/src/main/java/ProxyImage.java
@@ -0,0 +1,19 @@
+
+import processing.core.PImage;
+
+public class ProxyImage extends PImage {
+	private final String filename;
+	private PImage image;
+
+	public ProxyImage(final String name) {
+		filename = name;
+	}
+
+	@Override
+	public PImage get() {
+		if (image == null) {
+			image = Main.self.loadImage(filename);
+		}
+		return image;
+	}
+}
