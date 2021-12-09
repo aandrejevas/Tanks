d5ac8b81b63d83661483ddaa799d5b3faf4a48fc
AAndrejevas <aandrejevas5@gmail.com>
2021-12-09 22:43:05 +0200

    tyrimas

--- Tanks/src/main/java/client/FlyweightFactory.java
+++ Tanks/src/main/java/client/FlyweightFactory.java
@@ -4,7 +4,7 @@ import processing.core.PImage;
 import utils.Utils;
 
 public class FlyweightFactory {
-	private final PImage t34_tank, t34_tank_highlited, tiger_tank, sherman_tank,
+	/*private final PImage t34_tank, t34_tank_highlited, tiger_tank, sherman_tank,
 		bullet_blue, bullet_red, bullet_normal,
 		background_box, water_box, lava_box, metal_box, wood_box,
 		drop_sammo, drop_mammo, drop_lammo,
@@ -12,10 +12,10 @@ public class FlyweightFactory {
 		drop_shealth, drop_mhealth, drop_lhealth,
 		bar_health, bar_shield,
 		bullet_blue_big, bullet_red_big, bullet_normal_big,
-		bullet_blue_big_selected, bullet_red_big_selected, bullet_normal_big_selected;
+		bullet_blue_big_selected, bullet_red_big_selected, bullet_normal_big_selected;*/
 
 	public FlyweightFactory() {
-		t34_tank = new ProxyImage("t-34.png");
+		/*t34_tank = new ProxyImage("t-34.png");
 		tiger_tank = new ProxyImage("tiger-1.png");
 		sherman_tank = new ProxyImage("sherman.png");
 		bullet_blue = new ProxyImage("Bullet_Blue_3.png");
@@ -43,10 +43,10 @@ public class FlyweightFactory {
 		bullet_normal_big = new ProxyImage("Bullet_Green_2_Big.png");
 		bullet_blue_big_selected = new ProxyImage("Bullet_Blue_3_selected.png");
 		bullet_red_big_selected = new ProxyImage("Bullet_Red_2_selected.png");
-		bullet_normal_big_selected = new ProxyImage("Bullet_Green_2_selected.png");
+		bullet_normal_big_selected = new ProxyImage("Bullet_Green_2_selected.png");*/
 	}
 
-	public PImage getImage(final byte key) {
+	/*public PImage getImage(final byte key) {
 		switch (key) {
 			case Utils.MAP_EMPTY: return background_box.get();
 			case Utils.MAP_BORDER: return metal_box.get();
@@ -77,6 +77,40 @@ public class FlyweightFactory {
 			case Utils.SELECTED_SHOT_NORMAL: return bullet_normal_big_selected.get();
 			case Utils.SELECTED_SHOT_RED: return bullet_red_big_selected.get();
 			case Utils.SELECTED_SHOT_BLUE: return bullet_blue_big_selected.get();
+			default: throw new NullPointerException();
+		}
+	}*/
+	public PImage getImage(final byte key) {
+		switch (key) {
+			case Utils.MAP_EMPTY: return Main.self.loadImage("t-34.png");
+			case Utils.MAP_BORDER: return Main.self.loadImage("tiger-1.png");
+			case Utils.MAP_WALL: return Main.self.loadImage("sherman.png");
+			case Utils.MAP_WATER: return Main.self.loadImage("Bullet_Blue_3.png");
+			case Utils.MAP_LAVA: return Main.self.loadImage("Bullet_Green_2.png");
+			case Utils.MAP_T34: return Main.self.loadImage("Bullet_Red_2.png");
+			case Utils.MAP_SHERMAN: return Main.self.loadImage("Backgound_box.png");
+			case Utils.MAP_T34H: return Main.self.loadImage("metal_box.png");
+			case Utils.MAP_TIGER: return Main.self.loadImage("wood_box.png");
+			case Utils.DROP_LAMMO: return Main.self.loadImage("lava_box.png");
+			case Utils.DROP_MAMMO: return Main.self.loadImage("pudle_box.png");
+			case Utils.DROP_SAMMO: return Main.self.loadImage("Drops/Ammo_Drop/Large_Ammo.png");
+			case Utils.DROP_LARMOR: return Main.self.loadImage("Drops/Ammo_Drop/Medium_Ammo.png");
+			case Utils.DROP_MARMOR: return Main.self.loadImage("Drops/Ammo_Drop/Small_Ammo.png");
+			case Utils.DROP_SARMOR: return Main.self.loadImage("Drops/Armor_Drop/Small_Armor.png");
+			case Utils.DROP_LHEALTH: return Main.self.loadImage("Drops/Armor_Drop/Medium_Armor.png");
+			case Utils.DROP_MHEALTH: return Main.self.loadImage("Drops/Armor_Drop/Large_Armor.png");
+			case Utils.DROP_SHEALTH: return Main.self.loadImage("Drops/Health_Drop/Large_Health.png");
+			case Utils.SHOT_NORMAL: return Main.self.loadImage("Drops/Health_Drop/Medium_Health.png");
+			case Utils.SHOT_RED: return Main.self.loadImage("Drops/Health_Drop/Small_Health.png");
+			case Utils.SHOT_BLUE: return Main.self.loadImage("tank_t34_highlighted.png");
+			case Utils.BIG_SHOT_NORMAL: return Main.self.loadImage("Health.png");
+			case Utils.BIG_SHOT_RED: return Main.self.loadImage("Shield.png");
+			case Utils.BIG_SHOT_BLUE: return Main.self.loadImage("Bullet_Blue_3_Big.png");
+			case Utils.HEALTH_ICON: return Main.self.loadImage("Bullet_Red_2_Big.png");
+			case Utils.SHIELD_ICON: return Main.self.loadImage("Bullet_Green_2_Big.png");
+			case Utils.SELECTED_SHOT_NORMAL: return Main.self.loadImage("Bullet_Blue_3_selected.png");
+			case Utils.SELECTED_SHOT_RED: return Main.self.loadImage("Bullet_Red_2_selected.png");
+			case Utils.SELECTED_SHOT_BLUE: return Main.self.loadImage("Bullet_Green_2_selected.png");
 			default: throw new NullPointerException();
 		}
 	}
